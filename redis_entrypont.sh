3d3aa3b860d90f1256440d96fe70bf14696e6bbe
Kevin Yuan <whyork@qq.com>
2022-08-23 19:15:45 +0800

    Update redis_entrypont.sh

--- redis_entrypont.sh
+++ redis_entrypont.sh
@@ -5,21 +5,19 @@ export REDIS_PORT=${REDIS_PORT:-6379}
 export REDIS_MAXMEMORY=${REDIS_MAXMEMORY:-1000MB}
 export REDIS_MAXMEMORY_POLICY=${REDIS_MAXMEMORY_POLICY:-allkeys-lru}
 if [ ! -f "/redisdata/redis.conf" ]; then
- echo "inital redis.conf file not exist, create new in /redisdata/redis.conf"
- cp /redis/redis.conf /redisdata/
+ 	echo "inital redis.conf file not exist, create new in /redisdata/redis.conf"
+ 	cp /redis/redis.conf /redisdata/
+	cd /redisdata/
+	sed -i "s/^bind .*$/bind $REDIS_BIND/g" redis.conf
+	sed -i "s/^#* *requirepass .*$/requirepass $REDIS_PASSWORD/" redis.conf
+	sed -i "s/^dir .*$/dir \/redisdata/" redis.conf
+	sed -i "s/^dbfilename .*$/dbfilename redisearch.rdb/" redis.conf
+	sed -i "s/^logfile .*$/logfile \/redisdata\/redis.log/" redis.conf
+	sed -i "s/^#* *maxmemory .*$/maxmemory $REDIS_MAXMEMORY/" redis.conf
+	sed -i "s/^#* *maxmemory-policy .*$/maxmemory-policy $REDIS_MAXMEMORY_POLICY/" redis.conf
+	sed -i "s/^# *loadmodule \/path\/to\/my_module.so/loadmodule \/redis\/redisearch.so FRISOINI \/friso\/friso.ini/" redis.conf
+	sed -i "s/^# *loadmodule \/path\/to\/other_module.so/loadmodule \/redis\/rejson.so/" redis.conf
 fi
-
-cd /redisdata/
-sed -i "s/^bind .*$/bind $REDIS_BIND/g" redis.conf
-sed -i "s/^#* *requirepass .*$/requirepass $REDIS_PASSWORD/" redis.conf
-sed -i "s/^dir .*$/dir \/redisdata/" redis.conf
-sed -i "s/^dbfilename .*$/dbfilename redisearch.rdb/" redis.conf
-sed -i "s/^logfile .*$/logfile \/redisdata\/redis.log/" redis.conf
-sed -i "s/^#* *maxmemory .*$/maxmemory $REDIS_MAXMEMORY/" redis.conf
-sed -i "s/^#* *maxmemory-policy .*$/maxmemory-policy $REDIS_MAXMEMORY_POLICY/" redis.conf
-sed -i "s/^# *loadmodule \/path\/to\/my_module.so/loadmodule \/redis\/redisearch.so/" redis.conf
-sed -i "s/^# *loadmodule \/path\/to\/other_module.so/loadmodule \/redis\/rejson.so/" redis.conf
-sed -i "s/^loadmodule \/redis\/rejson.so/loadmodule \/redis\/rejson.so\nloadmodule \/redis\/redis-roaring.so\n/" /etc/redis.conf
 
 if [ ! -n "$1" ] ;then
 	echo "start: redis-server /redisdata/redis.conf"
